859a7aa52b5162ccfb59d0cd6848672aa6dcb149
GittyAjay <rightmajay@gmail.com>
2021-04-29 13:01:25 +0530

    Otp and Dashboard design fix still facing issue but enought for now

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -8,68 +8,71 @@ import { View, Text, StyleSheet, Pressable, Button, Animated, Keyboard } from 'r
 import { scale, verticalScale, moderateScale } from 'react-native-size-matters';
 import Carousel from 'react-native-snap-carousel';
 import Slider from '../components/Slider';
-import { withTimingTransition } from 'react-native-redash'
 import { TextInput, TouchableOpacity } from 'react-native-gesture-handler';
-export default function Dashboard(props: { navigation: { pop: Function } }) {
+import { createKeyboardAwareNavigator } from 'react-navigation';
+import { onChange } from 'react-native-reanimated';
+export default function Dashboard(props: { navigation: { push: Function, navigate: Function } }) {
 
     const { LOGIN_VIEW_HEIGHT, ICON_SIZE, BUTTON_HEIGHT, DEFAUTL_SPACE, FONT_SMALL, FONT_LARGE, FONT_MID, HEIGHT, WIDTH, BORDER_RADIUS_CIRCULAR, INLINE_GAP, BORDER_RADIUS, BORDER_WIDTH } = Numericals();
-    const animaValue = useRef(new Animated.Value(HEIGHT)).current;
-    const [loginUi, setloginUi] = useState(false);
-    const [inputStatus, setInputStatus] = useState(false);
-    const [userName, setUserName] = useState('');
-    useEffect(() => {
-        setloginUi(false);
-        Animated.timing(animaValue, {
-            toValue: HEIGHT - LOGIN_VIEW_HEIGHT,
-            duration: 400,
-            useNativeDriver: true
-        }).start();
-    }, [])
+    const animaValue = useRef(new Animated.Value(HEIGHT - LOGIN_VIEW_HEIGHT)).current;
+    const [mobileNo, setMobileNo] = useState('');
+    const [loginHeading, setloginHeading] = useState("Let's get started! Enter your mobile number");
+    const [backIconStatus, setbackIconStatus] = useState(false);
+
+    // Keyboard.addListener('keyboardDidShow', () => {
+    //     setloginHeading('Enter Your Mobile Number');
+    //     setbackIconStatus(true);
+    // });
+
+    // Keyboard.addListener('keyboardDidHide', () => {
+    //     setloginHeading("Let's get started! Enter your mobile number");
+    //     setbackIconStatus(false);
+    // });
+
     function interpolateAnima() {
-        setloginUi(prev => !prev);
         Animated.spring(animaValue, {
             toValue: 0,
             useNativeDriver: true
         }).start();
     }
-    function back() {
-        setloginUi(false);
-        Keyboard.dismiss;
-        Animated.timing(animaValue, {
+
+    function backAnim() {
+        Keyboard.dismiss();
+        Animated.spring(animaValue, {
             toValue: HEIGHT - LOGIN_VIEW_HEIGHT,
-            duration: 400,
             useNativeDriver: true
         }).start();
     }
-
     return (
         <View style={styles.container}>
             <StatusBar color={Colors.PRIMARY} />
-            <Animated.View>
+            <Text style={{ margin: moderateScale(INLINE_GAP), fontSize: scale(FONT_LARGE), fontFamily: 'Museo700-Regular', color: Colors.WHITE }}>Talktor</Text>
+            <View style={{ marginBottom: moderateScale(LOGIN_VIEW_HEIGHT) }}>
                 <Slider />
-            </Animated.View>
+            </View>
 
-            <Animated.View style={{ transform: [{ translateY: animaValue }], ...StyleSheet.absoluteFillObject, paddingHorizontal: DEFAUTL_SPACE, backgroundColor: Colors.WHITE }}>
-                <View style={{ flex: 1, flexDirection: 'column', justifyContent: 'space-between', alignItems: 'stretch', paddingVertical: 10 }}>
+            <Animated.View style={{ transform: [{ translateY: animaValue }], ...StyleSheet.absoluteFillObject, paddingHorizontal: moderateScale(DEFAUTL_SPACE / 2), backgroundColor: Colors.WHITE }}>
+                <View style={{ flex: 1, flexDirection: 'column', justifyContent: 'space-between', alignItems: 'stretch', paddingVertical: moderateScale(DEFAUTL_SPACE) }}>
                     <View>
-                        <TouchableOpacity style={{ paddingVertical: DEFAUTL_SPACE, justifyContent: 'flex-start', alignItems: 'flex-start' }} onPress={() => back()}>
-                            <AIcon name="arrowleft" size={loginUi ? ICON_SIZE : 0} />
+                        <TouchableOpacity style={{ paddingVertical: moderateScale(DEFAUTL_SPACE), justifyContent: 'flex-start', alignItems: 'flex-start' }} onPress={() => backAnim()}>
+                            <AIcon name="arrowleft" size={backIconStatus ? ICON_SIZE : 0} />
                         </TouchableOpacity>
-                        <Text style={{ fontSize: FONT_MID, fontFamily: 'Museo700-Regular', paddingVertical: DEFAUTL_SPACE }}>Let's get started! Enter your mobile number</Text>
-                        <View style={{ flexDirection: 'row', justifyContent: 'flex-start', alignItems: 'center', padding: DEFAUTL_SPACE, borderColor: userName.length < 10 && userName.length > 0 ? Colors.RED : Colors.GREY.SIMPLE, borderStyle: 'solid', borderRadius: BORDER_RADIUS, borderWidth: BORDER_WIDTH, marginVertical: DEFAUTL_SPACE }}>
-                            <Text style={{ fontFamily: 'Museo700-Regular', fontSize: FONT_MID }} >+91</Text>
+                        <Text style={{ fontSize: scale(FONT_SMALL), fontFamily: 'Museo700-Regular', paddingVertical: moderateScale(DEFAUTL_SPACE) }}>{loginHeading}</Text>
+                        <View style={{ flexDirection: 'row', justifyContent: 'flex-start', alignItems: 'center', padding: moderateScale(DEFAUTL_SPACE), borderColor: Colors.GREY.SIMPLE, borderStyle: 'solid', borderRadius: BORDER_RADIUS, borderWidth: BORDER_WIDTH, marginVertical: moderateScale(DEFAUTL_SPACE) }}>
+                            <Text style={{ fontFamily: 'Museo700-Regular', fontSize: scale(FONT_MID) }} >+91</Text>
                             <AIcon name="down" size={ICON_SIZE} />
                             <TextInput placeholder="Mobile number"
-                                style={{ fontFamily: 'Museo700-Regular', fontSize: FONT_MID, flex: 1 }}
+                                style={{ fontFamily: 'Museo700-Regular', fontSize: scale(FONT_MID), flex: 1 }}
                                 maxLength={10}
                                 onPressIn={() => { interpolateAnima(); }}
-                                onChange={() => setInputStatus(true)}
                                 keyboardType="phone-pad"
-                                onChangeText={user => { setUserName(user); }} />
+                                onChangeText={user => { setMobileNo(user); }} />
                         </View>
                     </View>
-                    <Pressable style={({ pressed }) => [{ backgroundColor: inputStatus ? Colors.PRIMARY : Colors.GREY.LIGHT, justifyContent: 'center', alignItems: 'center', height: BUTTON_HEIGHT, flexDirection: 'row', transform: [{ scale: pressed ? 0.9 : 1 }] }]} focusable={true} >
-                        <Text style={{ fontFamily: 'Museo700-Regular', fontSize: FONT_MID, color: Colors.WHITE }}>Submit</Text>
+                    <Pressable style={[{ backgroundColor: Colors.PRIMARY, justifyContent: 'center', alignItems: 'center', height: BUTTON_HEIGHT, flexDirection: 'row' }]} onPress={() => {
+                        props.navigation.push('Otp');
+                    }}>
+                        <Text style={{ fontFamily: 'Museo700-Regular', fontSize: moderateScale(FONT_MID), color: Colors.WHITE }}>Submit</Text>
                     </Pressable>
                 </View>
             </Animated.View>
@@ -79,7 +82,7 @@ export default function Dashboard(props: { navigation: { pop: Function } }) {
 }
 const styles = StyleSheet.create({
     container: {
-        flex: 1, alignItems: 'stretch', justifyContent: 'center', flexDirection: 'column', backgroundColor: Colors.PRIMARY
+        flex: 1, alignItems: 'stretch', justifyContent: 'space-between', flexDirection: 'column', backgroundColor: Colors.PRIMARY
     },
 });
 
--- src/pages/Otp.tsx
+++ src/pages/Otp.tsx
@@ -7,37 +7,44 @@ import AIcon from 'react-native-vector-icons/AntDesign';
 import { TextInput, TouchableOpacity } from 'react-native-gesture-handler';
 import { scale, moderateScale } from 'react-native-size-matters';
 
-export default function Otp(props: { navigation: { pop: Function } }) {
-    const { ICON_SIZE, DEFAUTL_SPACE, FONT_LARGE, FONT_SMALL, INLINE_GAP, BORDER_WIDTH } = Numericals();
+export default function Otp(props: { navigation: { push: Function, pop: Function } }) {
+    const { ICON_SIZE, BUTTON_HEIGHT, DEFAUTL_SPACE, FONT_MID, FONT_LARGE, FONT_SMALL, INLINE_GAP, BORDER_WIDTH } = Numericals();
     const mobile_no = '6392363003';
     const [isActive, setActive] = useState(false);
     return (
-        <View style={[styles.container, { backgroundColor: Colors.WHITE, paddingHorizontal: moderateScale(INLINE_GAP) }]}>
+        <View style={[styles.container, { backgroundColor: Colors.WHITE }]}>
             <StatusBarTemplate color={Colors.WHITE} />
-            <View style={[styles.closeButton]}>
-                <TouchableOpacity onPress={() => { props.navigation.pop() }}>
-                    <AIcon name="arrowleft" color={Colors.BLACK} size={moderateScale(moderateScale(ICON_SIZE))} />
-                </TouchableOpacity>
-            </View>
-            <View style={[styles.heading]}>
-                <Text style={[styles.subheading_txt, { fontSize: scale(FONT_LARGE) }]}>Confirm your number</Text>
-            </View>
-            <View style={[styles.heading, { marginVertical: moderateScale(moderateScale(DEFAUTL_SPACE / 2)) }]}>
-                <Text style={[styles.subheading_txt, { fontSize: scale(FONT_SMALL), color: Colors.BLACK }]}>Enter the code we have sent by SMS to {mobile_no}:</Text>
-            </View>
-            <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
-                <Pressable style={({ pressed }) => [{ borderRadius: moderateScale(DEFAUTL_SPACE), borderColor: isActive ? Colors.BLACK : Colors.GREY.SIMPLE, borderWidth: BORDER_WIDTH, width: scale(60), height: scale(60) }, styles.shape]}><TextInput style={{ fontSize: scale(FONT_LARGE) }} maxLength={1} onChange={() => setActive(true)} onBlur={() => setActive(false)} /></Pressable>
-                <Pressable style={({ pressed }) => [{ borderRadius: moderateScale(DEFAUTL_SPACE), borderColor: isActive ? Colors.BLACK : Colors.GREY.SIMPLE, borderWidth: BORDER_WIDTH, marginHorizontal: DEFAUTL_SPACE, justifyContent: 'center', width: scale(60), height: scale(60) }, , styles.shape]}><TextInput style={{ fontSize: scale(FONT_LARGE) }} maxLength={1} /></Pressable>
-                <Pressable style={({ pressed }) => [{ borderRadius: moderateScale(DEFAUTL_SPACE), borderColor: isActive ? Colors.BLACK : Colors.GREY.SIMPLE, borderWidth: BORDER_WIDTH, width: scale(60), height: scale(60) }, styles.shape]}><TextInput style={{ fontSize: scale(FONT_LARGE) }} maxLength={1} /></Pressable>
-                <Pressable style={({ pressed }) => [{ borderRadius: moderateScale(DEFAUTL_SPACE), borderColor: isActive ? Colors.BLACK : Colors.GREY.SIMPLE, borderWidth: BORDER_WIDTH, marginHorizontal: DEFAUTL_SPACE, justifyContent: 'center', width: scale(60), height: scale(60) }, , styles.shape]}><TextInput style={{ fontSize: scale(FONT_LARGE) }} maxLength={1} /></Pressable>
-            </View>
-            <View style={{ flexDirection: 'row', marginVertical: moderateScale(moderateScale(DEFAUTL_SPACE / 2)) }}>
-                <View style={{ flexDirection: 'row' }}>
-                    <Text style={[styles.subheading_txt, { fontSize: scale(FONT_SMALL), color: Colors.BLACK }]}>Haven't received a code?</Text>
+            <View style={{ paddingHorizontal: moderateScale(INLINE_GAP) }}>
+                <View style={[styles.closeButton]}>
+                    <TouchableOpacity onPress={() => { props.navigation.pop() }} style={{ marginBottom: moderateScale(DEFAUTL_SPACE) }}>
+                        <AIcon name="arrowleft" color={Colors.BLACK} size={moderateScale(moderateScale(ICON_SIZE))} />
+                    </TouchableOpacity>
                 </View>
-                <TouchableOpacity onPress={() => console.log()}>
-                    <Text style={{ borderBottomColor: Colors.BLACK, borderStyle: 'solid', borderBottomWidth: 2, color: Colors.BLACK, fontSize: scale(FONT_SMALL), fontFamily: 'Comfortaa-Bold', marginLeft: DEFAUTL_SPACE / 2 }}>More Options</Text>
-                </TouchableOpacity>
+                <View style={[styles.heading]}>
+                    <Text style={[styles.subheading_txt, { fontSize: scale(FONT_LARGE) }]}>Confirm your number</Text>
+                </View>
+                <View style={[styles.heading, { marginVertical: moderateScale(moderateScale(DEFAUTL_SPACE / 2)) }]}>
+                    <Text style={[styles.subheading_txt, { fontSize: scale(FONT_SMALL), color: Colors.BLACK }]}>Enter the code we have sent by SMS to {mobile_no}:</Text>
+                </View>
+                <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
+                    <Pressable style={({ pressed }) => [{ borderRadius: moderateScale(DEFAUTL_SPACE), borderColor: isActive ? Colors.BLACK : Colors.GREY.SIMPLE, borderWidth: BORDER_WIDTH, width: scale(60), height: scale(60) }, styles.shape]}><TextInput style={{ fontSize: scale(FONT_LARGE) }} maxLength={1} onChange={() => setActive(true)} onBlur={() => setActive(false)} /></Pressable>
+                    <Pressable style={({ pressed }) => [{ borderRadius: moderateScale(DEFAUTL_SPACE), borderColor: isActive ? Colors.BLACK : Colors.GREY.SIMPLE, borderWidth: BORDER_WIDTH, marginHorizontal: DEFAUTL_SPACE, justifyContent: 'center', width: scale(60), height: scale(60) }, , styles.shape]}><TextInput style={{ fontSize: scale(FONT_LARGE) }} maxLength={1} /></Pressable>
+                    <Pressable style={({ pressed }) => [{ borderRadius: moderateScale(DEFAUTL_SPACE), borderColor: isActive ? Colors.BLACK : Colors.GREY.SIMPLE, borderWidth: BORDER_WIDTH, width: scale(60), height: scale(60) }, styles.shape]}><TextInput style={{ fontSize: scale(FONT_LARGE) }} maxLength={1} /></Pressable>
+                    <Pressable style={({ pressed }) => [{ borderRadius: moderateScale(DEFAUTL_SPACE), borderColor: isActive ? Colors.BLACK : Colors.GREY.SIMPLE, borderWidth: BORDER_WIDTH, marginHorizontal: DEFAUTL_SPACE, justifyContent: 'center', width: scale(60), height: scale(60) }, , styles.shape]}><TextInput style={{ fontSize: scale(FONT_LARGE) }} maxLength={1} /></Pressable>
+                </View>
+                <View style={{ flexDirection: 'row', marginVertical: moderateScale(moderateScale(DEFAUTL_SPACE / 2)) }}>
+                    <View style={{ flexDirection: 'row' }}>
+                        <Text style={[styles.subheading_txt, { fontSize: scale(FONT_SMALL), color: Colors.BLACK }]}>Haven't received a code?</Text>
+                    </View>
+                    <TouchableOpacity onPress={() => console.log()}>
+                        <Text style={{ borderBottomColor: Colors.BLACK, borderStyle: 'solid', borderBottomWidth: 2, color: Colors.BLACK, fontSize: scale(FONT_SMALL), fontFamily: 'Museo700-Regular', marginLeft: DEFAUTL_SPACE / 2 }}>More Options</Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
+            <View style={{ padding: moderateScale(DEFAUTL_SPACE / 2) }}>
+                <Pressable style={({ pressed }) => [{ backgroundColor: Colors.PRIMARY, justifyContent: 'center', alignItems: 'center', height: BUTTON_HEIGHT, flexDirection: 'row', transform: [{ scale: pressed ? 0.9 : 1 }] }]} onPress={() => props.navigation.push('Home')}>
+                    <Text style={{ fontFamily: 'Museo700-Regular', fontSize: moderateScale(FONT_MID), color: Colors.WHITE }}>Submit</Text>
+                </Pressable>
             </View>
         </View>
     )
@@ -46,8 +53,8 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         flexDirection: 'column',
-        justifyContent: 'flex-start',
-        alignItems: 'flex-start',
+        justifyContent: 'space-between',
+        alignItems: 'stretch',
     },
     closeButton: {
         alignSelf: 'flex-start',
@@ -56,10 +63,10 @@ const styles = StyleSheet.create({
         alignSelf: 'flex-start',
     },
     heading_txt: {
-        fontFamily: 'Comfortaa-Bold',
+        fontFamily: 'Museo700-Regular',
     },
     subheading_txt: {
-        fontFamily: 'Comfortaa-Bold',
+        fontFamily: 'Museo700-Regular',
     },
     shape: {
         borderStyle: 'solid',
